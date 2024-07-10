0c4c44ba7cf00db4c9fe3145f67a0710e0194b89
Yunus <yunusabdulhameed1@gmail.com>
2024-07-10 09:45:31 +0100

    constructio of the responsive navbar

---
+++ src/main.js
@@ -0,0 +1,16 @@
+let openBtn = document.querySelector(".bi-list");
+let closeBtn = document.querySelector(".bi-x-lg");
+let menu = document.querySelector(".navbar__menu")
+
+openBtn.addEventListener("click", ()=>{
+    openBtn.classList.toggle("hide");
+    closeBtn.classList.toggle("hide");
+    menu.classList.toggle("hidden");
+})
+
+closeBtn.addEventListener("click", ()=>{
+    openBtn.classList.toggle("hide");
+    closeBtn.classList.toggle("hide");
+    menu.classList.toggle("hidden");
+
+})
